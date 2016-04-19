85c9c7b01685e9cfb8a52d71495efb8640510071
Jay Canty <jcanty@fuzzproductions.com>
2016-04-18 23:24:06 -0400

    Change heading

--- ImageManager/PersistanceManager.swift
+++ ImageManager/PersistanceManager.swift
@@ -1,17 +1,9 @@
 //
-//  PersistanceManager.swift
+//  ImageManager.swift
 //  ImageManager
 //
 //  Created by Jay Canty on 4/18/16.
 //  Copyright © 2016 Jay Canty. All rights reserved.
-//
-
-//
-//  PersistanceManager.swift
-//  YMCA-Y-MVP
-//
-//  Created by Jay Canty on 9/1/15.
-//  Copyright (c) 2015 FuzzProducitons. All rights reserved.
 //
 
 import Foundation
